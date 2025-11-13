ed269fc11fb7ce4052f7d9f99387ae242a66d95d
sahydi <sahydi@protonmail.com>
2025-11-12 21:37:33 -0300

    webpack sass ts-loader ts-node-dev and babel

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import dotenv from 'dotenv';
+
+const envFile = dotenv.config().parsed || {};
+
+const envKeys = Object.keys(envFile).reduce((acc, key) => {
+  acc[`process.env.${key}`] = JSON.stringify(envFile[key]);
+  return acc;
+}, {});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: {
+    background: './src/app/utils/background.ts',
+    app: './src/app/app.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    ...createHtmlPlugins(['app']),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: '.' },
+        { from: 'src/assets', to: 'assets' },
+      ],
+    }),
+    new webpack.DefinePlugin(envKeys),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+};
+
+function createHtmlPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', `${chunk}.html`),
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
